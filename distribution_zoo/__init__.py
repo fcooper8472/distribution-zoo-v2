9b266744a18ef093c7590e159457cf36a522b5fb
Fergus Cooper <fcooper8472@gmail.com>
2023-12-24 17:24:39 +0000

    Add a distribution class to help map to distributions

--- distribution_zoo/__init__.py
+++ distribution_zoo/__init__.py
@@ -1,5 +1,7 @@
 from .base_distribution import BaseDistribution
 
+from .distribution_class import DistributionClass
+
 from .utils import get_random_animal_emoji
 from .utils import inject_custom_css
 from .utils import language_display_name
