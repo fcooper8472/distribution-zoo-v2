145c659533398c21743b3e6a37b750a1cc425f93
Fergus Cooper <fcooper8472@gmail.com>
2023-12-24 14:23:06 +0000

    Add gamma distribution skeleton

--- distribution_zoo/cont_uni/gamma.py
+++ distribution_zoo/cont_uni/gamma.py
@@ -1,4 +1,10 @@
-from ..base_distribution import BaseDistribution
+from distribution_zoo import BaseDistribution
+
+import altair as alt
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+import streamlit as st
 
 
 class Gamma(BaseDistribution):
@@ -7,3 +13,12 @@ class Gamma(BaseDistribution):
 
     def __init__(self):
         super().__init__()
+
+    def sliders(self):
+        pass
+
+    def plot(self):
+        pass
+
+    def update_code_substitutions(self):
+        pass
