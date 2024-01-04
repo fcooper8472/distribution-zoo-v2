2e9ed74f905f879091f328351fd8986957b51862
Fergus Cooper <fcooper8472@gmail.com>
2024-01-04 21:55:46 +0000

    Update menu items

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
+        'Get Help': 'https://github.com/fcooper8472/distribution-zoo-v2/issues',
+        'Report a bug': "https://github.com/fcooper8472/distribution-zoo-v2/issues",
+        'About': "Explore the Distribution Zoo"
     }
 )
 
